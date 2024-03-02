9ba027575ddc34990d4b77d9bd0d8e6dbdec8378
Mohammad Almomany <mohammad.w.momani@gmail.com>
2024-03-02 11:13:48 +0300

    Problem 1 - Fill Matrix With Random Numbers.

--- Problem #1 - Fill Matrix/Problem #1 - Fill Matrix.cpp
+++ Problem #1 - Fill Matrix/Problem #1 - Fill Matrix.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
+#include<iomanip>// this library stored the std::setw
+using namespace std;
+
+int RandomNumber(int From, int To) {
+    // Function to generate a random number
+    int randNum = 1 + rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+
+void FillMatrix(int arr[3][3]) {
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            arr[i][j] = RandomNumber(1, 100);
+        }
+    }
+}
+
+void PrintMatrix(int arr[3][3]) {
+    
+    cout << "\n The following is a 3x3 random matrix:\n";
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << setw(3) << arr[i][j] << "\t";
+        }
+        cout << '\n';
+    }
+    cout << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    srand((unsigned)time(NULL));
+
+    int arr[3][3] = {};
+
+    FillMatrix(arr);
+
+    PrintMatrix(arr);
+
+    return 0;
 }
